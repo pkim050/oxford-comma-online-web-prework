a47215df19176c5b899970369e9254e571e58889
Patrick Kim <pkim050@gmail.com>
2019-01-31 16:02:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,6 @@
 def oxford_comma(array)
   last = array.pop()
+  return last.to_s if array.size == 0
+  return array.join(" and ") + last.to_s if array.size == 1
   array.join(", ") + ", and " + last.to_s
 end
